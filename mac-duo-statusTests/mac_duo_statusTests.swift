9fa9d51994ec16e97a836827ee45ea4bc40b7630
豕豕豕 <70371454+lliooly@users.noreply.github.com>
2026-09-14 19:29:00 +0800

    Initial Commit

---
+++ mac-duo-statusTests/mac_duo_statusTests.swift
@@ -0,0 +1,19 @@
+//
+//  mac_duo_statusTests.swift
+//  mac-duo-statusTests
+//
+//  Created by shishishi on 9/14/26.
+//
+
+import Testing
+@testable import mac_duo_status
+
+struct mac_duo_statusTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+        // Swift Testing Documentation
+        // https://developer.apple.com/documentation/testing
+    }
+
+}
